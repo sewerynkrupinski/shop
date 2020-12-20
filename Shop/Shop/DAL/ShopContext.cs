4d72a8e3f6aec69a10d9fe9e1a36a114543cc204
Seweryn Krupiński <45992679+sewerynkrupinski@users.noreply.github.com>
2020-12-20 14:42:32 +0100

    Added dbcontext

---
+++ Shop/Shop/DAL/ShopContext.cs
@@ -0,0 +1,29 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Shop.DAL
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext() : base("ShopContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPosition> OrderPositions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
